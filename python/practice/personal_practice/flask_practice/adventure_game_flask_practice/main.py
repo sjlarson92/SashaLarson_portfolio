0b8420fe6163a365cd3aca0d0aebb83250074436
Sasha Larson <sjlarson92@gmail.com>
2018-10-09 21:20:27 -0500

    added registration validation and error messages

--- python/practice/personal_practice/flask_practice/adventure_game_flask_practice/main.py
+++ python/practice/personal_practice/flask_practice/adventure_game_flask_practice/main.py
@@ -39,13 +39,79 @@ def about():
 @app.route('/register', methods=["GET","POST"])
 def register():
     return render_template("register2.html")
-    
+
     #the code below is for the register form with WTF
     # form = RegistrationForm()
     # if form.validate_on_submit():
     #     flash('Account Created', 'success')
     #     return redirect(url_for('home'))
     # return render_template("register.html", title="Register", form=form)
+
+@app.route('/register/error', methods=['post'])
+def register_error():
+    user = request.form.get('username')
+    password = request.form.get('password')
+    verify_password = request.form.get('verify_password')
+    email = request.form.get('email')
+
+    pattern = re.compile(r'^[a-zA-Z0-9]{3,20}$')
+    if pattern.match(user):
+        print('>>>>>There is a match for user!!!!!')
+
+        password_pattern = re.compile(r'^.{3,20}$')
+        if password_pattern.match(password):
+            print('>>>>>There is a match for password!!!!!')
+
+            if password == verify_password:
+                print('>>>>>>The passwords match')
+
+                if email == "":
+
+                    return render_template('login.html', user = user)
+
+            elif re.compile(r'\s').match(password):
+                error2 = "No spaces allowed in Password"
+
+                return render_template ('register2.html', error2 = error2)
+
+            else:
+                print('>>>>>>Passwords do not match:(')
+                error3 = "Passwords do not match"
+
+                return render_template('register2.html', user = user, error3 = error3)
+        else:
+            print('>>>>>>There is no match for password:(')
+            error2 = "Your password is incorrect"
+
+            return render_template('register2.html', user = user, error2 = error2)
+
+
+    else:
+        print('>>>>>>There is no match for user:(')
+
+        if user == "" and password == "" and verify_password == "":
+            error1 = "Please input a username"
+            error2 = "Please input password"
+            error3 = "Please verify password"
+
+            return render_template('register2.html', error1 = error1, error2 = error2, error3 = error3)
+
+        elif len(user) < 3 or len(user) > 20:
+            error1 = "Username must be between 3 to 20 characters long"
+
+            return render_template ('register2.html', error1 = error1)
+
+        elif re.compile(r'\s').match(user):
+            error1 = "No spaces allowed in Username"
+
+            return render_template ('register2.html', error1 = error1)
+
+        else:
+            error1 = "Your username is incorrect"
+
+            return render_template('register2.html', user = user, error1 = error1)
+
+    return render_template({{ url_for('login') }}, user = user, password = password, verify_password = verify_password, email = email)
 
 @app.route('/login', methods=["GET","POST"])
 def login():
