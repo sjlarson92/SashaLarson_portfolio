521fd86201540c11fc6e54319c396c06c24047bc
Sasha Larson <sjlarson92@gmail.com>
2018-12-02 18:07:16 -0600

    completed search controller result class

--- c#/lc101/assignments/TechJobsMVC/src/TechJobs/Controllers/SearchController.cs
+++ c#/lc101/assignments/TechJobsMVC/src/TechJobs/Controllers/SearchController.cs
@@ -1,3 +1,4 @@
+using System;
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
@@ -13,7 +14,35 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+          Console.WriteLine("This is the results method");
+          ViewBag.columns = ListController.columnChoices;
+          ViewBag.title = "Search";
+
+          Console.WriteLine(">>> The searchType is : " + searchType);
+
+          Console.WriteLine(">>> The seachTerm is : " + searchTerm);
+
+          if (searchType.Equals("all"))
+          {
+
+              List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+              ViewBag.resultsTitle =  "All " + searchTerm + " Results";
+              ViewBag.jobs = jobs;
+              return View("Index");
+          }
+          else
+          {
+            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.resultsTitle =  "All " + searchType + ":" + searchTerm + " Results";
+            //ViewBag.column = searchType;
+            ViewBag.jobs = jobs;
+            return View("Index");
+          }
+
+        }
+        // TODO #1 - Create a Results action method to process
         // search request and display results
 
     }
