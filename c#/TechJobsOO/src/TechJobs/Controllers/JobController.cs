a614629ce51faa262c17a259ff16e2e915d1ebeb
Sasha Larson <sjlarson92@gmail.com>
2018-12-16 11:44:58 -0600

    completed step 3 for TechJobsOO for data fields and validation

--- c#/TechJobsOO/src/TechJobs/Controllers/JobController.cs
+++ c#/TechJobsOO/src/TechJobs/Controllers/JobController.cs
@@ -23,7 +23,6 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
 
             Job currentJob = jobData.Find(id);
 
@@ -41,8 +40,12 @@ namespace TechJobs.Controllers
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
             // TODO #6 - Validate the ViewModel and if valid, create a
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+            //if (ModelState.IsValid)
+            //{
+              // new Job and add it to the JobData data store. Then
+              // redirect to the Job detail (Index) action/view for the new Job.
+            //}
+
 
             return View(newJobViewModel);
         }
--- c#/TechJobsOO/src/TechJobs/ViewModels/NewJobViewModel.cs
+++ c#/TechJobsOO/src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,12 +15,26 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
+        [Required]
+        public string Location { get; set;}
+
+        [Required]
+        [Display(Name = "Skill")]
+        public string CoreCompetency { get; set;}
+
+        [Required]
+        [Display(Name = "Position Type")]
+        public string PositionType { get; set;}
+
         // TODO #3 - Included other fields needed to create a job,
         // with correct validation attributes and display names.
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
+
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
+
         public List<SelectListItem> CoreCompetencies { get; set; } = new List<SelectListItem>();
+        
         public List<SelectListItem> PositionTypes { get; set; } = new List<SelectListItem>();
 
         public NewJobViewModel()
@@ -36,7 +50,7 @@ namespace TechJobs.ViewModels
                 });
             }
 
-            // TODO #4 - populate the other List<SelectListItem> 
+            // TODO #4 - populate the other List<SelectListItem>
             // collections needed in the view
 
         }
