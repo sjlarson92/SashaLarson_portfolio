bc29aad64b2f2531afaf5dd53ef9ee83b44c2860
Sasha Larson <sjlarson92@gmail.com>
2018-12-16 12:51:28 -0600

    completed adding drop down menu for Location and Skill for TechJobsOO

--- c#/TechJobsOO/src/TechJobs/ViewModels/NewJobViewModel.cs
+++ c#/TechJobsOO/src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,28 +15,31 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
+        //check if these need to use a LocationID to find options
         [Required]
-        public string Location { get; set;}
+        [Display(Name = "Location")]
+        public int LocationID { get; set;}
 
         [Required]
         [Display(Name = "Skill")]
-        public string CoreCompetency { get; set;}
+        public int CoreCompetencyID { get; set;}
 
         [Required]
         [Display(Name = "Position Type")]
-        public string PositionType { get; set;}
+        public int PositionTypeID { get; set;}
 
-        // TODO #3 - Included other fields needed to create a job,
-        // with correct validation attributes and display names.
 
+
+        //Create empty SelectListItem for each field
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
 
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
 
         public List<SelectListItem> CoreCompetencies { get; set; } = new List<SelectListItem>();
-        
+
         public List<SelectListItem> PositionTypes { get; set; } = new List<SelectListItem>();
 
+        //this gets the data to put in the list of SelectListItem
         public NewJobViewModel()
         {
 
@@ -48,6 +51,23 @@ namespace TechJobs.ViewModels
                     Value = field.ID.ToString(),
                     Text = field.Value
                 });
+            }
+            foreach (Location field in jobData.Locations.ToList())
+            {
+              Locations.Add(new SelectListItem{
+                  Value = field.ID.ToString(),
+                  Text = field.Value
+                });
+
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+              CoreCompetencies.Add(new SelectListItem{
+                  Value = field.ID.ToString(),
+                  Text = field.Value
+                });
+
             }
 
             // TODO #4 - populate the other List<SelectListItem>
