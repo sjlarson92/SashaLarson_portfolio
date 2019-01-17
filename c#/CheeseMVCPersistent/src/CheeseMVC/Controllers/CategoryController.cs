20d67875258832af503d9b5d744c28f11ec0d523
Sasha Larson <sjlarson92@gmail.com>
2019-01-16 20:52:54 -0600

    added Index action to CategoryController

--- c#/CheeseMVCPersistent/src/CheeseMVC/Controllers/CategoryController.cs
+++ c#/CheeseMVCPersistent/src/CheeseMVC/Controllers/CategoryController.cs
@@ -15,5 +15,15 @@ namespace CheeseMVC.Controllers
       {
         context = dbContext;
       }
+      // This creates a private field context of type CheeseDbContext. This object will be the mechanism with which we interact with objects stored in the database. The MVC framework will do the work of creating an instance of CheeseDbContext and passing it into our controller's constructor.
+      //
+      // This code would need to be added to each controller class that you want to have access to the persistent collections defined within CheeseDbContext.
+      public IActionResult Index()
+      {
+          // List<Cheese> cheeses = context.Cheeses.ToList();
+
+          return View;
+      }
+
     }
 }
