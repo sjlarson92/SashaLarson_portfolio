c09df69959bd3b4a1b1b064d86e87fdc0057825b
Sasha Larson <sjlarson92@gmail.com>
2019-02-25 22:16:52 -0600

    created Menu Controller

---
+++ c#/CheeseMVCPersistent/src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private CheeseDbContext context;
+
+        public CheeseController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
--- c#/CheeseMVCPersistent/src/CheeseMVC/Data/CheeseDbContext.cs
+++ c#/CheeseMVCPersistent/src/CheeseMVC/Data/CheeseDbContext.cs
@@ -17,6 +17,7 @@ namespace CheeseMVC.Data
 
         public DbSet<CheeseMenu> CheeseMenus {get; set; }
 
+        //This method will set the primary key of the CheeseMenu class and table to be a composite key, consisting of both CheeseID and MenuID.
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CheeseMenu>()
