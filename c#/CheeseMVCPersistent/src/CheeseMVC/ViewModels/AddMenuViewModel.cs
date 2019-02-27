827addc9f8807f1c5f206cb9720977e493a5c9e0
Sasha Larson <sjlarson92@gmail.com>
2019-02-26 18:58:58 -0600

    created AddmenuViewModel and added Name property

---
+++ c#/CheeseMVCPersistent/src/CheeseMVC/ViewModels/AddMenuViewModel.cs
@@ -0,0 +1,20 @@
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddMenuViewModel
+    {
+        [Required]
+        [Display(Name = "Menu Name")]
+        public string Name { get; set; }
+
+
+        }
+    }
+}
