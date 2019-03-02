b95b08abe72e04cf929c4596b7a168cadcd22f5d
Sasha Larson <sjlarson92@gmail.com>
2019-03-02 17:16:22 -0600

    fixed menu index view to display @menu.Name

--- c#/CheeseMVCPersistent/src/CheeseMVC/Migrations/20190302230916_AddMenu.Designer.cs
+++ c#/CheeseMVCPersistent/src/CheeseMVC/Migrations/20190302230916_AddMenu.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190302230916_AddMenu")]
+    partial class AddMenu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
