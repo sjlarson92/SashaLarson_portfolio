1a214814fbbaaa35df3fd2c757514dee3495600e
Sasha Larson <sjlarson92@gmail.com>
2019-03-02 13:55:38 -0600

    created AddMenuItemViewModel viewmodel and added constructors

---
+++ c#/CheeseMVCPersistent/src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -0,0 +1,34 @@
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddMenuItemViewModel
+    {
+
+        public int cheeseID { get; set; }
+
+        public int menuID { get; set; }
+
+        [Required]
+        public Menu Menu { get; set; }
+
+        [Required]
+        public List<SelectListItem> Cheeses { get; set; }
+
+        public AddMenuItemViewModel(){} //empty default constructor
+
+        public AddMenuItemViewModel(Menu menu, IEnumerable<Cheese> cheese){
+          Cheeses.Add(new SelectListItem
+          {
+              Value = cheese.ID.ToString(),
+              Text = cheese.Name
+          });
+        }
+    }
+}
