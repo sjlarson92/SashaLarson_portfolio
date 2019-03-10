20b528129b1e730825eaaae8d69a752a87476907
Sasha Larson <sjlarson92@gmail.com>
2019-03-10 13:21:14 -0500

    created advgame database and players table

---
+++ python/AdventureGame_project/config.py
@@ -0,0 +1,12 @@
+import MySQLdb
+import re
+from flask import render_template
+
+#this should contain the database connection string
+
+# DB connection
+# my_connection = MySQLdb.connect(host='localhost',
+#                              database='blogz',
+#                              user='sasha',
+#                              password='password')
+# cursor = my_connection.cursor()
