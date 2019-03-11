fceaee04fc9c10996ac9fd911c703acca8366d95
Sasha Larson <sjlarson92@gmail.com>
2019-03-10 19:40:37 -0500

    updated run.py and added templates to organize app flow

--- python/AdventureGame_project/config.py
+++ python/AdventureGame_project/config.py
@@ -2,9 +2,11 @@ import MySQLdb
 import re
 from flask import render_template
 
+SECRET_KEY = '_5#y2L"F4Q8z\n\xec]/'
+
 #this should contain the database connection string
 
-DB connection
+#DB connection
 my_connection = MySQLdb.connect(host='localhost',
                              database='advgame',
                              user='sasha',
--- python/AdventureGame_project/run.py
+++ python/AdventureGame_project/run.py
@@ -1,3 +1,12 @@
 from flask import Flask
+import MySQLdb
 
 app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
+
+db = MySQLdb(app)
+#module object is not callable
+
+if __name__ == "__main__":
+    app.run(debug = True)
