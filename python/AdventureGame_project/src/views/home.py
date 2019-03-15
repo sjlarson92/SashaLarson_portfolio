4ea769abfab7b9f969be19b6441423dadc90a845
Sasha Larson <sjlarson92@gmail.com>
2019-03-14 22:18:14 -0500

    fixed titles

--- python/AdventureGame_project/src/views/home.py
+++ python/AdventureGame_project/src/views/home.py
@@ -1,21 +1,22 @@
 from flask import render_template, request, redirect, url_for, flash
 from src import app
 
+title='ADVENTURE GAME'
 
 @app.route('/')
 def index():
     # return 'Hello World!'
-    return render_template('/Home/index.html', title='ADVENTURE GAME')
+    return render_template('/Home/index.html', title=title)
 
 @app.route('/Intro', methods = ['GET', 'POST'])
 def intro():
     print(">>>>this is the info route")
     if "startButton" in request.form:
         print(">>> You clicked the startButton")
-        return render_template('/Intro/playerNum.html')
+        return render_template('/Intro/playerNum.html', title=title)
     elif "instructions" in request.form:
         print(">>> You clicked the instructions")
-        return render_template('/Intro/instructions.html')
+        return render_template('/Intro/instructions.html', title="How to Play")
 
 
     # button = request.form.get('name')
