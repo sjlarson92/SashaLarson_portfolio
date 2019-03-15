ca2ff4c99cecb77e0e746599082a6d8922a6b441
Sasha Larson <sjlarson92@gmail.com>
2019-03-14 22:14:54 -0500

    created routes to buttons for start game and how to play

--- python/AdventureGame_project/src/views/home.py
+++ python/AdventureGame_project/src/views/home.py
@@ -1,7 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from src import app
+
 
 @app.route('/')
 def index():
     # return 'Hello World!'
     return render_template('/Home/index.html', title='ADVENTURE GAME')
+
+@app.route('/Intro', methods = ['GET', 'POST'])
+def intro():
+    print(">>>>this is the info route")
+    if "startButton" in request.form:
+        print(">>> You clicked the startButton")
+        return render_template('/Intro/playerNum.html')
+    elif "instructions" in request.form:
+        print(">>> You clicked the instructions")
+        return render_template('/Intro/instructions.html')
+
+
+    # button = request.form.get('name')
+    # print('>>>>>>this is the name of the button: ', button)
+    #
+    # if name == return render_template("")
