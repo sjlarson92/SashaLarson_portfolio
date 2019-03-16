c0fb972caf54a232041fbeebda5225decc03561b
Sasha Larson <sjlarson92@gmail.com>
2019-03-16 12:54:59 -0500

    added playerNum route to home controller

--- python/AdventureGame_project/src/views/home.py
+++ python/AdventureGame_project/src/views/home.py
@@ -13,8 +13,12 @@ def intro():
     print(">>> You clicked the startButton")
     return render_template('/Intro/playerNum.html', title=title)
 
-
 @app.route('/HowtoPlay')
 def instructions():
     print(">>> You clicked the instructions")
     return render_template('/Intro/instructions.html', title="How to Play")
+
+@app.route('/Intro/PlayerName', methods=['POST'])
+def playername():
+    print(">>> This is the player name input page")
+    return render_template('/Intro/playerName.html', title="Player Names")
