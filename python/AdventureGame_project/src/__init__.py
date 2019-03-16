9f11e87b1429250218196b44a26f2378b79105d0
Sasha Larson <sjlarson92@gmail.com>
2019-03-16 12:43:10 -0500

    fixed route path for start and instructions to be separate forms

--- python/AdventureGame_project/src/__init__.py
+++ python/AdventureGame_project/src/__init__.py
@@ -4,14 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 import src.views.home
-# app.config.from_pyfile('config.py')
-#
-# db = MySQLdb(app)
-# #module object is not callable
-
-# @app.route('/')
-# def index():
-#     return render_template('Home/index.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
--- python/AdventureGame_project/src/views/home.py
+++ python/AdventureGame_project/src/views/home.py
@@ -10,16 +10,11 @@ def index():
 
 @app.route('/Intro', methods = ['GET', 'POST'])
 def intro():
-    print(">>>>this is the info route")
-    if "startButton" in request.form:
-        print(">>> You clicked the startButton")
-        return render_template('/Intro/playerNum.html', title=title)
-    elif "instructions" in request.form:
-        print(">>> You clicked the instructions")
-        return render_template('/Intro/instructions.html', title="How to Play")
+    print(">>> You clicked the startButton")
+    return render_template('/Intro/playerNum.html', title=title)
 
 
-    # button = request.form.get('name')
-    # print('>>>>>>this is the name of the button: ', button)
-    #
-    # if name == return render_template("")
+@app.route('/HowtoPlay')
+def instructions():
+    print(">>> You clicked the instructions")
+    return render_template('/Intro/instructions.html', title="How to Play")
