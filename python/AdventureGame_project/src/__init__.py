6550fbb2a5c38068a62d99d89b21db9027f15706
Sasha Larson <sjlarson92@gmail.com>
2019-03-23 16:51:16 -0500

    fixed db connection string and added chapter column to players table

--- python/AdventureGame_project/src/__init__.py
+++ python/AdventureGame_project/src/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-# import MySQLdb
+import re
+
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] ='_5#y2L"F4Q8z\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
 
 from src.views.home import home
 from src.views.game import game
--- python/AdventureGame_project/src/models/players.py
+++ python/AdventureGame_project/src/models/players.py
@@ -1,2 +1,24 @@
-class Player:
-    
+# class Player(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(20), unique=True, nullable=False)
+#     choice = db.Column(db.Boolean, )
+
+import MySQLdb
+import re
+from flask import render_template
+
+# DB connection
+my_connection = MySQLdb.connect(host='localhost',
+                             database='advgame',
+                             user='sasha',
+                             password='password')
+cursor = my_connection.cursor()
+
+class User:
+    def __init__(self, playerName):
+        # print("this is init function in example class")
+        self.id = 0
+        self.playerName = playerName
+        self.choice = null
+        self.dead = False
+        self.chapter = 1
