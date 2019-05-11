7ffabcf2e294819c710a2e27082a00f3b7c7b528
Sasha Larson <sjlarson92@gmail.com>
2019-05-11 15:19:07 -0500

    test

--- react/hello-world-with-yarn/src/index.js
+++ react/hello-world-with-yarn/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App => <h1>{"Hello World"}</h1>
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
--- wiki/javascript/react.md
+++ wiki/javascript/react.md
@@ -1,3 +1,15 @@
 to install dependency use yarn: `yarn install dependency-name`
 
 do not push dependencies to gitHub, these are installed by using `yarn` or `yarn install` and looking at the yarn.lock file
+
+run app with `yarn start` and you must be in the app folder
+
+the html file in a react app should utilize just one div within the body tags.
+
+render always takes a component NOT a function
+
+```
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(App(), document.getElementById('root'));
+```
