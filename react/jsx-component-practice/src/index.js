78dafbf4423dc85dec5c5e02f9ab79413eaa969d
Sasha Larson <sjlarson92@gmail.com>
2019-05-13 22:58:11 -0500

    created character list component to render and loop through list and display names

--- react/jsx-component-practice/src/index.js
+++ react/jsx-component-practice/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello World</h1>
+let gotCharacters = [
+  {
+    name: "Jon Snow",
+    pet: "Direwolf"
+
+  },
+  {
+    name: "Cersei Lannister",
+    pet: "Jamie Lannister"
+  },
+  {
+    name: "Arya Stark",
+    pet: "A girl has no pet"
+  },
+  {
+    name: "Danearys Targaryean",
+    pet: "Dragon"
+  }];
+
+let gotList = gotCharacters.map(function(character){
+  return(<li>{character.name}</li>);
+})
+
+const App = () =>
+<div>
+  <h1>Game of Thrones Deadpool</h1>
+  <h2>Characters:</h2>
+  <ul>{gotList}</ul>
+</div>
 
 ReactDOM.render(<App/>, document.getElementById('root'));
