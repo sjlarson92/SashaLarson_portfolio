f4c40406875c1bafec42f267ca7b5840bc8cf9a1
Sasha Larson <sjlarson92@gmail.com>
2019-08-19 21:51:20 -0500

    added css font color to result text

--- react/coin-toss/src/components/Text.js
+++ react/coin-toss/src/components/Text.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Text = (props) => {
   let coinResult = props.coinTossResult;
   let userWinLoss = props.userWinLoss;
+  let textDiv;
+
+  if (userWinLoss === "won"){
+    textDiv = "win";
+  }
+  else{
+    textDiv = "loss";
+  }
 
   if (userWinLoss){
     return (
-      <div>Rolled {coinResult}! You {userWinLoss}</div>
+      <div id={textDiv}>Rolled {coinResult}! You {userWinLoss}</div>
     )
   }
   else {
