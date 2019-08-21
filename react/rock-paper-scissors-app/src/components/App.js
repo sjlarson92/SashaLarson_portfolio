4be4338df1ccefc63b22859dea2a8d2acb7d5b36
Sasha Larson <sjlarson92@gmail.com>
2019-08-20 20:56:24 -0500

    created rock-paper-scissors-app

---
+++ react/rock-paper-scissors-app/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userChoice: ""
+    };
+  }
+render(){
+  return (
+    <div>Hello!</div>
+  )
+}
+
+}
+
+export default App;
