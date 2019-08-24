3b1cdd507244bba0c2ea01962b6575cb76a9b7fb
Lucas Costa <lucas.gomes.costa@gmail.com>
2019-08-24 16:15:47 -0500

    refactoring

---
+++ react/heads-or-scissors-games/__tests__/HeadsOrScissors.test.js
@@ -0,0 +1,8 @@
+describe('checkIfUserWon', () => {
+  it('should return true if userChoice equals randomValueFromArray', () => {
+
+  })
+  it('should return false if userChoice equals randomValueFromArray', () => {
+
+  })
+})
--- react/heads-or-scissors-games/src/HeadsOrScissors.js
+++ react/heads-or-scissors-games/src/HeadsOrScissors.js
@@ -6,13 +6,29 @@ import { images } from './data.js'
 class HeadsOrScissors extends React.Component {
   state = {
     userChoice: '',
-    endGameMessage: ''
+    endGameMessage: '',
+  }
+
+  checkIfUserWon = (randomValue, userChoice) => {
+    return randomValue === userChoice && true;
+  }
+
+  getRandomValueFromArray = (anArray) => {
+    return anArray[Math.floor(Math.random() * anArray.length)];
   }
 
   handleImageClick = (userChoice) => {
+    const imageNames = images.map(image => image.name)
+    const randomValue = this.getRandomValueFromArray(imageNames);
+    const userWon = this.checkIfUserWon(randomValue, userChoice);
+
+    const endGameMessage = userWon
+      ? `Computer rolled ${randomValue}. You won`
+      : `Computer rolled ${randomValue}. You lost`
+
     this.setState({
       userChoice,
-      endGameMessage: `You chose ${userChoice}`
+      endGameMessage
     })
 
   }
