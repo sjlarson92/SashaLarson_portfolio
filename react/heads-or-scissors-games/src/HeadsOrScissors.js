84f33c3f5e8cae6164adf7e0abf1a858e72556a8
Lucas Costa <lucas.gomes.costa@gmail.com>
2019-08-25 14:21:31 -0500

    finished game example

--- react/heads-or-scissors-games/src/HeadsOrScissors.js
+++ react/heads-or-scissors-games/src/HeadsOrScissors.js
@@ -1,54 +1,94 @@
 import React from 'react';
 
-import Image from './components/Image'
-import { images } from './data.js'
+import GameLayout from './components/GameLayout'
+import { headsOrTailsImages, rockPaperScissorsImages, rockPaperScissorsTypes as RPSTypes } from './data.js'
+
+const types = {
+  HEADSTAILS: 'heads-tails',
+  ROCKPAPERSCISSORS: 'rock-paper-scissors'
+}
+
+const winStatusTypes = {
+  WIN: 'WIN',
+  LOSE: 'LOSE',
+  TIE: 'TIE'
+}
+
+const endGameMessagesMap = {
+  WIN: 'You won',
+  LOSE: 'You lost',
+  TIE: 'You tied'
+}
 
 class HeadsOrScissors extends React.Component {
   state = {
-    userChoice: '',
-    endGameMessage: '',
+    resultForHeadsTails: '',
+    resultForRockPaperScissors: ''
   }
 
-  checkIfUserWon = (randomValue, userChoice) => {
-    return randomValue === userChoice && true;
+  checkIfUserWonHeadsTails = (userChoice, randomValue) =>
+    userChoice === randomValue ? winStatusTypes.WIN : winStatusTypes.LOSE;
+
+  checkIfUserWonRockPaperScissors = (userChoice, randomValue) => {
+    if (userChoice === randomValue) {
+      return winStatusTypes.TIE;
+    }
+    else if ((userChoice === RPSTypes.ROCK && randomValue === RPSTypes.PAPER) ||
+             (userChoice === RPSTypes.PAPER && randomValue === RPSTypes.SCISSORS) ||
+             (userChoice === RPSTypes.SCISSORS && randomValue === RPSTypes.ROCK)) {
+      return winStatusTypes.LOSE;
+    }
+    else {
+      return winStatusTypes.WIN;
+    }
   }
 
   getRandomValueFromArray = (anArray) => {
     return anArray[Math.floor(Math.random() * anArray.length)];
   }
 
-  handleImageClick = (userChoice) => {
-    const imageNames = images.map(image => image.name)
+  handleImageClick = (userChoice, imagesArray, gameType) => {
+    const imageNames = imagesArray.map(image => image.name)
     const randomValue = this.getRandomValueFromArray(imageNames);
-    const userWon = this.checkIfUserWon(randomValue, userChoice);
+    let userWon;
 
-    const endGameMessage = userWon
-      ? `Computer rolled ${randomValue}. You won`
-      : `Computer rolled ${randomValue}. You lost`
-
-    this.setState({
-      userChoice,
-      endGameMessage
-    })
-
+    if (gameType === types.HEADSTAILS) {
+       userWon = this.checkIfUserWonHeadsTails(userChoice, randomValue);
+       const resultForHeadsTails = `Computer rolled ${randomValue}.
+         ${endGameMessagesMap[userWon]}`;
+       this.setState({
+         resultForHeadsTails,
+       })
+    } else if (gameType === types.ROCKPAPERSCISSORS) {
+      userWon = this.checkIfUserWonRockPaperScissors(userChoice, randomValue);
+      const resultForRockPaperScissors = `Computer picked ${randomValue}.
+        ${endGameMessagesMap[userWon]}`;
+      this.setState({
+        resultForRockPaperScissors,
+      })
+    }
   }
 
-  displayImageComponent = () =>
-    images.map(image =>
-     <Image
-      key={image.id}
-      image={image}
-      onClick={() => this.handleImageClick(image.name)}
-    />)
-
   render(){
-    const { userChoice, endGameMessage } = this.state;
+    const { resultForHeadsTails, resultForRockPaperScissors } = this.state;
     return (
       <div>
-        <h1>Heads or Tails</h1>
-        <h3>Click one</h3>
-        {this.displayImageComponent()}
-        <p>{endGameMessage}</p>
+        <GameLayout
+          header="Heads or Tails"
+          gameType={types.HEADSTAILS}
+          instructions="Click one"
+          imagesArray={headsOrTailsImages}
+          endGameMessage={resultForHeadsTails}
+          onClick={this.handleImageClick}
+          />
+          <GameLayout
+            header="Rock, Paper, Scissors"
+            gameType={types.ROCKPAPERSCISSORS}
+            instructions="Pick one"
+            imagesArray={rockPaperScissorsImages}
+            endGameMessage={resultForRockPaperScissors}
+            onClick={this.handleImageClick}
+            />
       </div>
     );
   }
