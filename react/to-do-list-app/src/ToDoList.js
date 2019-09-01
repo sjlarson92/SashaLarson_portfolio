88bcdf67c3a1b8c1f4ee08ff3f4e03ffd187a8da
Sasha Larson <sjlarson92@gmail.com>
2019-09-01 18:04:29 -0500

    removed components to code from top to bottom

--- react/to-do-list-app/src/ToDoList.js
+++ react/to-do-list-app/src/ToDoList.js
@@ -1,20 +1,52 @@
 import React from 'react';
 
 import {todos} from './data.js'
-import ToDoListLayout from './components/ToDoListLayout.js'
 
 class ToDoList extends React.Component {
   state = {
+    className: 'todo',
+    checked: false
+  }
 
+  updateCheckboxState = async () => {
+    await this.setState({checked: !this.state.checked});
+  }
+
+  updateClassNameState = () => {
+    const{checked} = this.state;
+    checked ? this.setState({className: 'todo'}) : this.setState({className: 'todo complete'});
+  }
+  handleCheckboxClick = () => {
+    this.updateCheckboxState()
+    this.updateClassNameState()
   }
 
   render() {
+    const{className, checked} = this.state;
     return (
-      <div>
-        <ToDoListLayout
-        header="Things To Do:"
-        tasksArray={todos}
-        />
+      <div className="app">
+        <h1>Things to do</h1>
+        <div
+        id="main-todo-list"
+        className="todo-list"
+        >
+          {todos.map(task =>
+            <div key={task.id} className={className}>
+              <input
+              type="checkbox"
+              className="todo-checkbox"
+              onChange={this.handleCheckboxClick}
+              defaultChecked={checked}
+              />
+              <span
+              className='todo-text'>
+              {task.text}
+              </span>
+            </div>
+          )}
+        </div>
+        <input type="text" placeholder="New todo"/>
+        <p><span id="remaining-count">{todos.length}</span> items remain</p>
       </div>
     )
   }
