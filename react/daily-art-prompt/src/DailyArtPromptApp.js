01b490463046bbaf60ec6e75a0a284eed1410adf
Sasha Larson <sjlarson92@gmail.com>
2019-10-11 23:10:55 -0500

    added test functions to buttons

--- react/daily-art-prompt/src/DailyArtPromptApp.js
+++ react/daily-art-prompt/src/DailyArtPromptApp.js
@@ -12,6 +12,20 @@ class DailyArtPromptApp extends React.Component {
 
   }
 
+  handleImageClick = () => {
+    console.log("Image was clicked");
+  }
+
+  handleButtonClick = (num) => {
+
+    if (num === -1){
+      console.log("Previous button was clicked");
+    }
+    else {
+      console.log("Next button was clicked");
+    }
+  }
+
   render(){
     return (
       <div className="app">
@@ -22,21 +36,21 @@ class DailyArtPromptApp extends React.Component {
         </div>
         <div className="prompt-row">
           <div>
-            <button>Previous</button>
+            <button onClick={() => this.handleButtonClick(-1)}>Previous</button>
           </div>
           <div className="prompt">
             <div className="padding">
               {this.state.date}
             </div>
             <div className="padding">
-              {this.state.prompt.id}
+              Prompt #{this.state.prompt.id}
             </div>
             <div className="text padding">
               {this.state.prompt.text}
             </div>
           </div>
           <div>
-            <button>Next</button>
+            <button onClick={() => this.handleButtonClick()}>Next</button>
           </div>
         </div>
         <hr></hr>
@@ -45,7 +59,7 @@ class DailyArtPromptApp extends React.Component {
         </div>
         <div className="row">
           <div className="column">
-            <img src={beachPuppy} alt="Puppy on the beach"/>
+            <img src={beachPuppy} alt="Puppy on the beach" onClick={() => this.handleImageClick()}/>
           </div>
           <div className="column">
             <img src={beagle} alt="beagle puppy"/>
