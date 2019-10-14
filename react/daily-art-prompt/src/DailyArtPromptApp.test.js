a74478d3b0baff72c936f96ba818178f8637be74
Sasha Larson <sjlarson92@gmail.com>
2019-10-14 17:50:45 -0500

    added test for findPromptbyId()

--- react/daily-art-prompt/src/DailyArtPromptApp.test.js
+++ react/daily-art-prompt/src/DailyArtPromptApp.test.js
@@ -7,4 +7,29 @@ describe('<DailyArtPromptApp>', () => {
   const wrapper = shallow(<DailyArtPromptApp />);
   expect(wrapper).toHaveLength(1);
   });
+
+})
+
+describe('findPromptbyId', () => {
+  it('returns a prompt obj when given a promptId', () => {
+    const wrapper = shallow(<DailyArtPromptApp />);
+    const instance = wrapper.instance();
+    const result = instance.findPromptbyId(1);
+    expect(result).toEqual({id: 1, date: "October 11", text: "Puppy"})
+  })
+})
+
+describe('findImagebyId', () => {
+  it('returns and img obj when given an imageId', () => {
+    const wrapper = shallow(<DailyArtPromptApp />);
+    const instance = wrapper.instance();
+    const result = instance.findImagebyId(1);
+    expect(result).toEqual({
+      id: 1,
+      promptId: 1,
+      src: beachPuppy,
+      name: 'beachPuppy',
+      liked: false
+    })
+  })
 })
