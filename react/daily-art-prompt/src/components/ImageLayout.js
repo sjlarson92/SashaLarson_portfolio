94f581d1eb2f75ae2a5bc0fa8fd8bf3d6b815835
Sasha Larson <sjlarson92@gmail.com>
2019-11-02 19:16:20 -0500

    refactored ImageLayout component

--- react/daily-art-prompt/src/components/ImageLayout.js
+++ react/daily-art-prompt/src/components/ImageLayout.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Image from './Image.js'
 
-const ImageLayout = ({onDoubleClick, image}) => {
-
-  const getClassName = (liked) => {
-    const className = liked ? "" : "hidden";
-    return className
-  }
-
-  const likedClassName = getClassName(image.liked)
-
-  return (
-    <div className="column">
-      <Image
-        image={image}
-        onDoubleClick={() => onDoubleClick(image.id)}
-      />
-      <div testID="likedDiv" className={likedClassName}>
-        Liked
-      </div>
+const ImageLayout = ({onDoubleClick, image}) =>
+  <div className="column">
+    <Image
+      image={image}
+      onDoubleClick={() => onDoubleClick(image.id)}
+    />
+    <div
+      testID="likedDiv" className={image.liked ? "" : "hidden"}>
+      Liked
     </div>
-  )
-}
-
+  </div>
 
 export default ImageLayout;
