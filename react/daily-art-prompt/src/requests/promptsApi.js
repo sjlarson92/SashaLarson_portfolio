52470006579ce0f69e01722d5fcaebd6fbfbb696
Sasha Larson <sjlarson92@gmail.com>
2019-12-10 19:48:33 -0600

    get prompt from api call 3 times

--- react/daily-art-prompt/src/requests/promptsApi.js
+++ react/daily-art-prompt/src/requests/promptsApi.js
@@ -1,22 +1,28 @@
 import axios from 'axios'
+import _ from 'lodash'
 import * as TYPES from '../store/actions'
 
 export const getPromptsAction = () => (dispatch) => {
-    axios.get('https://the-cocktail-db.p.rapidapi.com/list.php?i=list', {
-        main: 'GET',
-        headers: {
-            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-            "x-rapidapi-key": "2b67ca4689mshf62ddb622772e77p114945jsn275c8aee2894"
-        }
+    let i = 0
+    _.times(3, () =>
+        axios.get('https://the-cocktail-db.p.rapidapi.com/list.php?i=list', {
+            main: 'GET',
+            headers: {
+                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                "x-rapidapi-key": "2b67ca4689mshf62ddb622772e77p114945jsn275c8aee2894"
+            }
 
-    })
-        .then((response) => {
-            console.log('response is: ', response.data.drinks[0].strIngredient1)
-            dispatch({
-                type: TYPES.SET_INITIAL_PROMPTS,
-                payload: {
-                    text: response.data.drinks[0].strIngredient1
-                }
-            })
         })
+            .then((response) => {
+                console.log('response is: ', response.data.drinks[0].strIngredient1)
+                dispatch({
+                    type: TYPES.SET_INITIAL_PROMPTS,
+                    payload: {
+                        text: response.data.drinks[i].strIngredient1
+                    }
+                })
+                i += 1
+            })
+    )
 }
+
