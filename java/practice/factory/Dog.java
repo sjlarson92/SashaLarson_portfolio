8ba8878f65ff59e6ef49212933188f442ea1ffa0
Sasha Larson <sjlarson92@gmail.com>
2019-12-18 22:39:57 -0600

    java practice with object orientation

---
+++ java/practice/factory/Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    // any attributes - min: firstName, lastName, age, breed [specific type (enum,
+    // not a string)], isGoodDog
+    // any getters/setters/ maybe other stuff?
+
+    // 1- no args should have funny first/last names, breed: beagle, isGoodDog false
+
+}
---
+++ java/practice/factory/Factory.java
@@ -0,0 +1,11 @@
+public class Factory {
+    public static void main(String[] args) {
+        // 1- (Display) Create a Dog obj, without any args (no args contructor)
+        // 2- (Allow user to) Create another dog, all args
+        // User should see the dog 2's new data
+        // 3- (Allow user to) Create a DIFFERENT dog with all args
+        // User should see the dog 3's new data
+        // 4- do some dog comparisons
+        // min req: show me oldest dog, all good dogs
+    }
+}
