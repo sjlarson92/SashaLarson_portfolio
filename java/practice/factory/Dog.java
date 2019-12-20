ed7d68e184c51842404e8fad176d06ab209696ae
Sasha Larson <sjlarson92@gmail.com>
2019-12-19 21:50:26 -0600

    created default constructor for Dog

--- java/practice/factory/Dog.java
+++ java/practice/factory/Dog.java
@@ -1,4 +1,21 @@
 public class Dog {
+    enum Breed {
+        BEAGLE, MUTT, WESTIE
+    }
+
+    String firstName;
+    String lastName;
+    int age;
+    Boolean isGoodDog;
+    Breed breed;
+
+    public Dog() {
+        this.firstName = "HeWhoShall";
+        this.lastName = "NotBeNamed";
+        this.age = 90;
+        this.isGoodDog = false;
+        this.breed = Breed.BEAGLE;
+    }
     // any attributes - min: firstName, lastName, age, breed [specific type (enum,
     // not a string)], isGoodDog
     // any getters/setters/ maybe other stuff?
--- java/practice/factory/Factory.java
+++ java/practice/factory/Factory.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Factory {
+
     public static void main(String[] args) {
+        Dog myDog = new Dog();
+        System.out.println("\nfirstName: " + myDog.firstName + "\nlastName: " + myDog.lastName + "\n Age is: "
+                + myDog.age + "\n isGoodBoy: " + myDog.isGoodDog + "\n Breed is: " + myDog.breed);
+
         // 1- (Display) Create a Dog obj, without any args (no args contructor)
         // 2- (Allow user to) Create another dog, all args
         // User should see the dog 2's new data
