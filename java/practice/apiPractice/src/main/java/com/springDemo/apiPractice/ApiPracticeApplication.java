559488649f50f871cb2d835ccc672ddac4c8e70b
Sasha Larson <sjlarson92@gmail.com>
2019-12-25 14:16:51 -0600

    initial commit for spring practice project

---
+++ java/practice/apiPractice/src/main/java/com/springDemo/apiPractice/ApiPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.springDemo.apiPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticeApplication.class, args);
+		System.out.println("Spring boot is fun");
+	}
+
+}
---
+++ java/practice/apiPractice/src/test/java/com/springDemo/apiPractice/ApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDemo.apiPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
