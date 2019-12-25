5ea636e3a4e2cc635a5aaf1cd00e18d2a19ef524
Sasha Larson <sjlarson92@gmail.com>
2019-12-25 14:42:09 -0600

    able to call method in controller from service

--- java/practice/apiPractice/src/main/java/com/springDemo/apiPractice/controller/DogController.java
+++ java/practice/apiPractice/src/main/java/com/springDemo/apiPractice/controller/DogController.java
@@ -1,7 +1,12 @@
 package com.springDemo.apiPractice.controller;
 
+import com.springDemo.apiPractice.service.DogService;
+
 public class DogController {
+    DogService dogService = new DogService();
+
     public void test() {
         System.out.println("my computer is so slow");
+        dogService.test();
     }
 }
---
+++ java/practice/apiPractice/src/main/java/com/springDemo/apiPractice/service/DogService.java
@@ -0,0 +1,7 @@
+package com.springDemo.apiPractice.service;
+
+public class DogService {
+    public void test() {
+        System.out.println("I am hungry :D");
+    }
+}
