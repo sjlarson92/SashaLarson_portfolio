de01b508cc163c2dc15d3660829c3dc459fd183f
Sasha Larson <sjlarson92@gmail.com>
2020-01-02 19:47:17 -0600

    wiki for computer set up

--- java/DailyArtPrompt/src/main/java/com/dap/DailyArtPrompt/controller/ImageController.java
+++ java/DailyArtPrompt/src/main/java/com/dap/DailyArtPrompt/controller/ImageController.java
@@ -3,10 +3,9 @@ package com.dap.DailyArtPrompt.controller;
 import com.dap.DailyArtPrompt.service.ImageService;
 import com.dap.DailyArtPrompt.model.Image;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 
@@ -19,6 +18,7 @@ public class ImageController {
     public ResponseEntity<Image> allimages() {
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+
 
         Image responseBody = imageService.getImage();
 
---
+++ wiki/setUpComputer.md
@@ -0,0 +1,25 @@
+# Setting Up New Computer
+
+# Applications to Download:
+
+- Itsycal
+- Spectacle
+- 1Password
+- Sophos
+
+- iTerm2
+- VS Code Studio
+- IntelliJ
+- Postman
+- zsh
+- Homebrew
+- yarn
+- java
+- Link/Sync Github
+- Link Terminal Aliases (/dotfiles/.bash_profile)
+
+## 
+
+# Stop Mac from rearranging screen by most used
+
+- System Preferences -> Mission Control -> Uncheck box to 'Automatically rearrange spaces'
