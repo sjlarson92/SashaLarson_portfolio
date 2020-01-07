8648fa2390632268e1475fafa16ef8ccedc3db97
Sasha Larson <sjlarson92@gmail.com>
2020-01-07 00:15:10 -0600

    formatted code

--- react/daily-art-prompt/src/store/dispatchFunctions.js
+++ react/daily-art-prompt/src/store/dispatchFunctions.js
@@ -1,27 +1,30 @@
-import * as TYPES from '../store/actions'
-import moment from 'moment';
+import * as TYPES from "../store/actions";
+import moment from "moment";
 
 export const updateNextDateAction = () => (dispatch, getState) => {
-    const date = moment(getState().date).add(1, 'day').format('YYYY-MM-DD')
-    if(getState().prompts[date] !== undefined) {
-        dispatch({
-            type: TYPES.UPDATE_NEXT_DATE,
-            payload: {
-                date
-            }
-        })
-    }
-    
-}
+  const date = moment(getState().date)
+    .add(1, "day")
+    .format("YYYY-MM-DD");
+  if (getState().prompts[date] !== undefined) {
+    dispatch({
+      type: TYPES.UPDATE_NEXT_DATE,
+      payload: {
+        date
+      }
+    });
+  }
+};
 
 export const updatePreviousDateAction = () => (dispatch, getState) => {
-    const date = moment(getState().date).subtract(1, 'day').format('YYYY-MM-DD')
-    if(getState().prompts[date] !== undefined) {
-        dispatch({
-            type: TYPES.UPDATE_PREVIOUS_DATE,
-            payload: {
-                date
-            }
-        })
-    }
-}
+  const date = moment(getState().date)
+    .subtract(1, "day")
+    .format("YYYY-MM-DD");
+  if (getState().prompts[date] !== undefined) {
+    dispatch({
+      type: TYPES.UPDATE_PREVIOUS_DATE,
+      payload: {
+        date
+      }
+    });
+  }
+};
