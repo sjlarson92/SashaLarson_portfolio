14039aa0b5963a3f2ebdf4ce414969b3fe297f29
Sasha Larson <sjlarson92@gmail.com>
2020-01-18 20:06:50 -0600

    initial commit for rewards app

---
+++ java/practice/rewardsProgram/src/main/java/com/interviewQuestion/rewardsProgram/RewardsProgramApplication.java
@@ -0,0 +1,13 @@
+package com.interviewQuestion.rewardsProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsProgramApplication.class, args);
+	}
+
+}
---
+++ java/practice/rewardsProgram/src/main/java/com/interviewQuestion/rewardsProgram/controller/RewardsController.java
@@ -0,0 +1,10 @@
+package com.interviewQuestion.rewardsProgram.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rewards")
+public class RewardsController {
+
+}
