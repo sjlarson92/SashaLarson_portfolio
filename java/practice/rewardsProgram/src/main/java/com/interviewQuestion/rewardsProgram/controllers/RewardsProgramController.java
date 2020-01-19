ac41f4681e3ba1fb2d24a705243c70be23da775e
Sasha Larson <sjlarson92@gmail.com>
2020-01-19 11:51:55 -0600

    accept parameters in request

--- java/practice/rewardsProgram/src/main/java/com/interviewQuestion/rewardsProgram/controllers/RewardsProgramController.java
+++ java/practice/rewardsProgram/src/main/java/com/interviewQuestion/rewardsProgram/controllers/RewardsProgramController.java
@@ -1,16 +1,27 @@
 package com.interviewQuestion.rewardsProgram.controllers;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/rewards")
 public class RewardsProgramController {
 
     @GetMapping("")
-    public String getRewards() {
-        return "this is the getRewards route";
+    public String getRewards(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam double purchaseAmount,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+            ) {
+        return (
+                "firstName: " + firstName + " lastname: " + lastName + " purchaseAmount: " + purchaseAmount + " date: " + date
+        );
     }
 
 }
