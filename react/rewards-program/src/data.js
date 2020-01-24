289fc531128ed891ec135efcc457803206fde51d
Sasha Larson <sjlarson92@gmail.com>
2020-01-23 21:24:46 -0600

    created mock data

---
+++ react/rewards-program/src/data.js
@@ -0,0 +1,37 @@
+export const transactionBodies = [
+  {
+    id: 1,
+    firstName: "marco",
+    lastName: "polo",
+    purchaseAmount: 102.33,
+    date: "2020-02-19"
+  },
+  {
+    id: 2,
+    firstName: "martha",
+    lastName: "steward",
+    purchaseAmount: 10.25,
+    date: "2020-01-17"
+  },
+  {
+    id: 2,
+    firstName: "martha",
+    lastName: "steward",
+    purchaseAmount: 102.34,
+    date: "2020-02-16"
+  },
+  {
+    id: 1,
+    firstName: "marco",
+    lastName: "polo",
+    purchaseAmount: 51.33,
+    date: "2020-03-19"
+  },
+  {
+    id: 3,
+    firstName: "james",
+    lastName: "bond",
+    purchaseAmount: 100.33,
+    date: "2020-03-19"
+  }
+];
